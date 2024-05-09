f0ced980747bc3bff1e75d17d663c3e5e8270518
adrian <hansen.adrian.hardy@gmail.com>
2024-05-09 11:23:34 +0200

    Fix

---
+++ build/src/events/events.c.d
@@ -0,0 +1,5 @@
+build/./src/events/events.c.o: src/events/events.c src/events/events.h \
+ src/logger/logger.h src/device_table/device_table.h
+src/events/events.h:
+src/logger/logger.h:
+src/device_table/device_table.h:
---
+++ build/src/vnvic/interrupt_event.c.d
@@ -0,0 +1,6 @@
+build/./src/vnvic/interrupt_event.c.o: src/vnvic/interrupt_event.c \
+ src/vnvic/interrupt_event.h src/logger/logger.h \
+ src/device_table/device_table.h
+src/vnvic/interrupt_event.h:
+src/logger/logger.h:
+src/device_table/device_table.h:
